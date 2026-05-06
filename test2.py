d70f9f36658c1b4824a1a0e92d91b0a06841e36c
Andrew <75088114+andrew878d@users.noreply.github.com>
2026-05-06 15:31:32 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,32 +1,21 @@
 import board
 import busio
 import digitalio
-from PIL import Image, ImageDraw
 from adafruit_rgb_display import ili9341
+from PIL import Image, ImageDraw
 
-# 1. Setup Pins (Using the same ones as your current code)
-cs_pin = digitalio.DigitalInOut(board.D7) # GPIO 7 is CE1
-dc_pin = digitalio.DigitalInOut(board.D24)
-reset_pin = digitalio.DigitalInOut(board.D25)
+# 1. Pin Setup
+cs_pin = digitalio.DigitalInOut(board.D7)  # Physical Pin 26
+dc_pin = digitalio.DigitalInOut(board.D24) # Physical Pin 18
+reset_pin = digitalio.DigitalInOut(board.D25) # Physical Pin 22
 
-# 2. Setup SPI
+# 2. SPI Setup (Slower speed for stability)
 spi = busio.SPI(clock=board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 
-# 3. Initialize Display
-disp = ili9341.ILI9341(
-    spi,
-    rotation=90,
-    cs=cs_pin,
-    dc=dc_pin,
-    rst=reset_pin,
-    baudrate=24000000,
-)
+# 3. Initialize
+disp = ili9341.ILI9341(spi, rotation=90, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=8000000)
 
-# 4. Create Canvas & Draw
-image = Image.new("RGB", (320, 240), "BLACK")
-draw = ImageDraw.Draw(image)
-draw.rectangle([20, 20, 80, 80], outline="red", fill="blue")
-
-# 5. Push to Screen
+# 4. Draw a test Square
+image = Image.new("RGB", (disp.width, disp.height), "BLUE") # Should turn the whole screen blue
 disp.image(image)
-print("ILI9341 Test Sent.")
+print("Screen should now be Blue!")
