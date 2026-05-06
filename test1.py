755a22d890c5531027f61005ca3671215ed53bea
Andrew <75088114+andrew878d@users.noreply.github.com>
2026-05-06 15:21:26 -0700

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,10 @@
 import time
 from PIL import Image, ImageDraw
-import st7789 
+import ST7789 
 # Change to ILI9341 if that is your specific driver
 
 # 1. Initialize Display
-disp = st7789.st7789(port=0, cs=1, dc=24, rst=25, width=320, height=240, rotation=0)
+disp = ST7789.ST7789(port=0, cs=1, dc=24, rst=25, width=320, height=240, rotation=0)
 disp.begin()
 
 # 2. Create Canvas (Black background)
